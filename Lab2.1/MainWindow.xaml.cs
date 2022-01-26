a011c10b03d59e7d08961a8a7e43016246706876
Сергей Храмцов <vocmarh@mail.ru>
2022-01-26 17:55:09 +0500

    Добавьте файлы проекта.

---
+++ Lab2.1/MainWindow.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Lab2._1
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            Button button = new Button();
+            button.Content = "123456";
+            grid.Children.Add(button);
+        }
+    }
+}
